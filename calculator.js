853236b06a13ed7df2d99e36777e48aabacdedd8
yalmazchaudhry <yalmazchaudhry1@gmail.com>
2023-01-11 01:43:38 +0500

    remove extra comments

--- calculator.js
+++ calculator.js
@@ -1,24 +1,17 @@
-let result = document.getElementById('inputData');
+let result = document.getElementById("inputData");
 
-let calculate=(number)=>{
-    result.value=result.value+number;
-}
+let calculate = (number) => {
+  result.value = result.value + number;
+};
 
-let Result=()=>{
+let Result = () => {
+  try {
+    result.value = eval(result.value);
+  } catch (err) {
+    result.value = "Invalid";
+  }
+};
 
-    try{
-        result.value=eval(result.value)
-    }catch(err){
-        result.value= "Invalid";
-
-    }
-}
-
-function clr1(){
-result.value="";
-}
-// array method//
-let array = [];
-calculate=(number)=>{
-    array.push(number);
+function clr1() {
+  result.value = "";
 }
